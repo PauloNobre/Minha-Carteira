183b2be238d2fe4e6aa2f7ce505662a831d625c7
Paulo Henrique <paulinho140@gmail.com>
2020-08-16 01:30:10 -0300

    Desabilitando Spring Security

--- src/main/java/com/minhacarteira/configs/SpringFoxConfig.java
+++ src/main/java/com/minhacarteira/configs/SpringFoxConfig.java
@@ -1,10 +1,15 @@
 package com.minhacarteira.configs;
 
+import java.util.Properties;
+
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +17,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+	
+	@Bean
+    public BuildProperties buildProperties() {
+        return new BuildProperties(new Properties());
+    }
 
 	@Bean
-    public Docket api() { 
+    public Docket api() throws Exception { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+	
+	private ApiInfo apiInfo() throws Exception {
+        return new ApiInfoBuilder()
+                .title("MINHA CARTEIRA")
+                .description("Documentação MINHA CARTEIRA")
+                .termsOfServiceUrl("")
+                .license("")
+                .licenseUrl("")
+                .version(buildProperties().getVersion())
+                .build();
     }
 }
